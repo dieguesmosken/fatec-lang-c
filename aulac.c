b4d601b38daab0de4a96a74f2dd8d120e7682b76
mosken.fatec <matheus.diegues@fatec.sp.gov.br>
2023-09-15 20:44:15 -0300

    first commit in c language

---
+++ aulac.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+    int x = 10;
+    float y = 1.1;
+    char c = 'a';
+	  printf("Hello World!\n");
+    printf("x = %d, y = %.2f, c = %c\n", x, y, c);
+
+
+    }
