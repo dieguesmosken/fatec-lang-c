8a5e39f789fca3cb82040fa7b6e4bd4072ed139b
mosken.fatec <matheus.diegues@fatec.sp.gov.br>
2023-09-15 21:11:36 -0300

    test 2

--- aulac.c
+++ aulac.c
@@ -7,8 +7,17 @@ int main(int argc, char *argv[]) {
     int x = 10;
     float y = 1.1;
     char c = 'a';
+    double d = 10.33;
 	  printf("Hello World!\n");
-    printf("x = %d, y = %.2f, c = %c\n", x, y, c);
+    printf("o conteudo de x = %d, y = %.2f, c = %c\n", x, y, c);
+    printf("o conteudo de d = %.2f\n", d);
+    printf("o endereco de x = %p, y = %p, c = %p\n", &x, &y, &c);
+    printf("o endereco de d = %p\n", &d);
+    printf("o tamanho de x = %d, y = %d, c = %d\n", sizeof(x), sizeof(y), sizeof(c));
+    printf("o tamanho de d = %d\n", sizeof(d));
+    printf("o tamanho de int = %d, float = %d, char = %d\n, double = %d\n", sizeof(int), 
+    sizeof(float), sizeof(char), sizeof(double));
 
+    system("pause");
 
     }
