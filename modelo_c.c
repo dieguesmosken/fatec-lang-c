38bcf567d60c1c49acab1b853cbaa041cd6d7426
matheuuuss <teuzinytbr@outlook.com>
2023-10-03 21:05:22 -0300

    teste

--- modelo_c.c
+++ modelo_c.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* rode este programa usando 
+o console pauser ou 
+adicione seu próprio getch, 
+system ("pause") ou loop de entrada */
 
 int main(int argc, char *argv[]) {
 
     system("pause");
 
-
-    }
+    return 0;
+}
